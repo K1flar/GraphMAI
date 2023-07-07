7313fc1d060803b5ff746dde1eba75f45d99a0f1
SlepoyShaman <ilushin905@gmail.com>
2023-07-07 13:44:47 +0300

    add test action

--- GraphMAI/Controllers/GraphStorageController.cs
+++ GraphMAI/Controllers/GraphStorageController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using GraphMAI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GraphMAI.Controllers
@@ -7,6 +7,17 @@ namespace GraphMAI.Controllers
     [ApiController]
     public class GraphStorageController : ControllerBase
     {
-        
+        [HttpPost]
+        public IActionResult Test()
+        {
+            var result = new List<GraphEdge>() {
+                new GraphEdge() { Id = 1, From = 0, To = 1 },
+                new GraphEdge() { Id = 2, From = 0, To = 2 },
+                new GraphEdge() { Id = 3, From = 2, To = 3 },
+                new GraphEdge() { Id = 4, From = 1, To = 2 }
+            };
+
+            return Ok(result);
+        }
     }
 }
--- GraphMAI/Models/GraphEdge.cs
+++ GraphMAI/Models/GraphEdge.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public int From { get; set; }
         public int To { get; set; }
-        public double Weight { get; set; }
+        public double Weight { get; set; } = 1;
 
         public int GraphId { get; set; }
         public GraphEntity? Graph { get; set; }
