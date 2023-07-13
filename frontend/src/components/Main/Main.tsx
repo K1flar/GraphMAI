87b204110afdcd0ffaf33e937086998e27e6efc1
k1flar <den1s-davydov@mail.ru>
2023-07-13 12:28:21 +0300

    k1flar: create ui save graph & select graph

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -1,19 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Option } from '../../types'
 import Select from '../UI/Select/Select'
 import Canvas from '../Canvas/Canvas'
 import Modal from '../Modal/Modal'
 import ModalSetGraph from './ModalSetGraph/ModalSetGraph'
+import SaveGraph from '../SaveGraph/SaveGraph'
 
 import cls from './Main.module.css'
 
 import Graph from '../../modules/Graph/Graph'
 import { parseAdjacencyMatrix, parseListOfAdjacency, parseListOfEdges } from '../../modules/Parser/Parser'
+import axios from 'axios'
 
 const Main = () => {
     let [graph, setGraph] = useState<Graph>(new Graph([]))
     let [isVisibleModal, setIsVisibleModal] = useState<boolean>(false)
     let [flag, setFlag] = useState<'m' | 'e' | 'l'>('m') // флаг по которому определяется задание графа
+    let [names, setNames] = useState<Option[]>([])
+
+    useEffect(() => {
+        const fetchNames = async () => {
+            try {
+            const data = await axios.get("http://localhost:7175/api/GetAll")
+            console.log(data)
+            } catch (e) {console.log(e)}
+        }
+        fetchNames()
+        
+        setNames([
+            {title: 'Graph #1', value: 1},
+            {title: 'Graph #2', value: 2},
+            {title: 'Graph #3', value: 3}
+        ])
+    }, [])
 
     let optionsSetGraph: Option[] = [
         { title: 'Матрица смежности', value: 'm' },
@@ -60,8 +79,10 @@ const Main = () => {
                     <div className={cls.options}>
                         <Select action={actionSelectSetGraph} options={optionsSetGraph} placeholder='Задание графа' />
                         <Select options={optionsSelectAlg} placeholder='Выбрать алгоритм' />
+                        <Select options={names} placeholder='Выбрать граф' />
                     </div>
                     <Canvas graph={graph} name='Выберете алгоритм' />
+                    <SaveGraph graph={graph}/>
                 </div>
             </main>
         </div>
